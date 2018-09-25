1930f7f8eb05db0fa54358e2c601a4b71e43160f
Uhron <avasfel@gmail.com>
2018-09-25 17:23:02 +0200

    some changes

--- aes/AES.py
+++ aes/AES.py
@@ -1,52 +1,26 @@
 
-##############		KEY		##############
 
-K = [	[0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [1,1,1,1,1,1,1,1], [1,1,1,1,1,1,1,1],
-		[1,1,1,1,1,1,1,1], [0,0,0,0,0,0,0,0], [1,1,1,1,1,1,1,1], [0,0,0,0,0,0,0,0],
-		[0,0,0,0,0,0,0,0], [1,1,1,1,1,1,1,1], [0,0,0,0,0,0,0,0], [1,1,1,1,1,1,1,1],
-		[1,1,1,1,1,1,1,1], [0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0], [1,1,1,1,1,1,1,1]]
+#############	 UTILE FUCTION 		#############
 
-##############		KEY		##############
+def int_to_bitarray(n):
+	return [int(digit) for digit in bin(n)[2:]]
 
-##############		AFINE MATRIX		##############
 
-A = [	[1,0,0,0,1,1,1,1],
-		[1,1,0,0,0,1,1,1],
-		[1,1,1,0,0,0,1,1],
-		[1,1,1,1,0,0,0,1],
-		[1,1,1,1,1,0,0,0],
-		[0,1,1,1,1,1,0,0],
-		[0,0,1,1,1,1,1,0],
-		[0,0,0,1,1,1,1,1]]
+def concatenate_list_data(list):
+	""" concatenate list data, concatenate all data in a list into one string
 
-b = [1,1,0,0,0,1,1,0]#nº 99 hex(63)
+	Returns string of concatenated data
+	"""
+	result = ''
 
-C = [	[0,0,1,0,0,1,0,1],
-		[1,0,0,1,0,0,1,0],
-		[0,1,0,0,1,0,0,1],
-		[1,0,1,0,0,1,0,0],
-		[0,1,0,1,0,0,1,0],
-		[0,0,1,0,1,0,0,1],
-		[1,0,0,1,0,1,0,0],
-		[0,1,0,0,1,0,1,0]]
+	for element in list:
 
-d = [1,0,1,0,0,0,0,0]#nº 160 hex(A0)
+		result += str(element)
 
-##############		AFINE MATRIX		##############
+	return result
 
-##############		MIX COLUMNS		##############
+#############	 UTILE FUCTION 		#############
 
-MC = [	[[1,0], [1,1], [0,1], [0,1]],
-		[[0,1], [1,0], [1,1], [0,1]],
-		[[0,1], [0,1], [1,0], [1,1]],
-		[[1,1], [0,1], [0,1], [1,0]]]
-
-MC_inv = [	[[1,1,1,0], [1,0,1,1], [1,1,0,1], [1,0,0,1]],
-			[[1,0,0,1], [1,1,1,0], [1,0,1,1], [1,1,0,1]],
-			[[1,1,0,1], [1,0,0,1], [1,1,1,0], [1,0,1,1]],
-			[[1,0,1,1], [1,1,0,1], [1,0,0,1], [1,1,1,0]]]
-
-##############		MIX COLUMNS		##############
 
 ##############		ARITMETIC		##############
 
@@ -75,6 +49,7 @@ class Aritmetic:
 
 		if fill == None:
 				fill = len(self.p_irreductible) - 1
+
 
 		while( len(p) < fill):
 				p.insert(0, 0)
@@ -124,9 +99,9 @@ class Aritmetic:
 
 			p = self.fill(p)
 
-			if p[0] == 1:
-				p[0] = 0
+			if p[0] == 1 and len(p) != 8:
 				p = self.xorit(p, self.substitution)
+				del p[0]
 
 		else:
 			while(len(p) > (len(self.p_irreductible) - 1)):
@@ -251,51 +226,104 @@ ar = Aritmetic()
 ##############		AITMETIC		##############
 
 
-def int_to_bitarray(n):
-	return [int(digit) for digit in bin(n)[2:]]
+##############		KEY		##############
 
+K = [	ar.fill(int_to_bitarray(int("54",16))), ar.fill(int_to_bitarray(int("68",16))), ar.fill(int_to_bitarray(int("61",16))), ar.fill(int_to_bitarray(int("74",16))),
+		ar.fill(int_to_bitarray(int("73",16))), ar.fill(int_to_bitarray(int("20",16))), ar.fill(int_to_bitarray(int("6D",16))), ar.fill(int_to_bitarray(int("79",16))),
+		ar.fill(int_to_bitarray(int("20",16))), ar.fill(int_to_bitarray(int("4B",16))), ar.fill(int_to_bitarray(int("75",16))), ar.fill(int_to_bitarray(int("6E",16))),
+		ar.fill(int_to_bitarray(int("67",16))), ar.fill(int_to_bitarray(int("20",16))), ar.fill(int_to_bitarray(int("46",16))), ar.fill(int_to_bitarray(int("75",16)))]
 
-def concatenate_list_data(list):
-	""" concatenate list data, concatenate all data in a list into one string
+##############		KEY		##############
 
-	Returns string of concatenated data
-	"""
-	result = ''
+##############		AFINE MATRIX		##############
 
-	for element in list:
+A = [	[1,0,0,0,1,1,1,1],
+		[1,1,0,0,0,1,1,1],
+		[1,1,1,0,0,0,1,1],
+		[1,1,1,1,0,0,0,1],
+		[1,1,1,1,1,0,0,0],
+		[0,1,1,1,1,1,0,0],
+		[0,0,1,1,1,1,1,0],
+		[0,0,0,1,1,1,1,1]]
 
-		result += str(element)
+b = [1,1,0,0,0,1,1,0]#nº 99 hex(63)
 
-	return result
+C = [	[0,0,1,0,0,1,0,1],
+		[1,0,0,1,0,0,1,0],
+		[0,1,0,0,1,0,0,1],
+		[1,0,1,0,0,1,0,0],
+		[0,1,0,1,0,0,1,0],
+		[0,0,1,0,1,0,0,1],
+		[1,0,0,1,0,1,0,0],
+		[0,1,0,0,1,0,1,0]]
 
+d = [1,0,1,0,0,0,0,0]#nº 160 hex(A0)
 
-def subBytes(x):
+##############		AFINE MATRIX		##############
+
+##############		MIX COLUMNS		##############
+
+MC = [	[[0,0,1,0],[0,0,1,1],[0,0,0,1],[0,0,0,1]],
+		[[0,0,0,1],[0,0,1,0],[0,0,1,1],[0,0,0,1]],
+		[[0,0,0,1],[0,0,0,1],[0,0,1,0],[0,0,1,1]],
+		[[0,0,1,1],[0,0,0,1],[0,0,0,1],[0,0,1,0]]]
+
+MC_inv = [	[[1,1,1,0],[1,0,1,1],[1,1,0,1],[1,0,0,1]],
+			[[1,0,0,1],[1,1,1,0],[1,0,1,1],[1,1,0,1]],
+			[[1,1,0,1],[1,0,0,1],[1,1,1,0],[1,0,1,1]],
+			[[1,0,1,1],[1,1,0,1],[1,0,0,1],[1,1,1,0]]]
+
+##############		MIX COLUMNS		##############
+
+def subBytes(x, inv):
 	""" subBytes function -->> x * v = 1 mod( ar.p_irreductible ) 
 	-->> y = A * v + b
 
 	return y
 	"""
-	v = ar.fit(ar.gcd(ar.p_irreductible, x))
 
 	Ax = []
-	
-	for p in A:
-		tmp = []
 
-		for i in range(len(v)):
-			tmp.append(v[-i-1] and p[i])
+	if not inv :
+		v = ar.fit(ar.gcd(ar.p_irreductible, x))
+		x = b
 
-		t = 0
-		for i in tmp:
-			t = t ^ i
-		Ax.append(t)
+		for p in A:
+			tmp = []
+
+			for i in range(len(v)):
+				tmp.append(v[-i-1] and p[i])
+
+			t = 0
+			for i in tmp:
+				t = t ^ i
+			Ax.append(t)
+
+	else:
+		v = x
+		x = d
+		
+		for p in C:
+			tmp = []
+
+			for i in range(len(v)):
+				tmp.append(v[-i-1] and p[i])
+
+			t = 0
+			for i in tmp:
+				t = t ^ i
+
+			Ax.append(t)
 	
-	Ax = ar.xorit(Ax, b)
+		print(Ax)
+		Ax = ar.fill(ar.gcd(ar.p_irreductible, Ax))
+		print("inverse -->> " + str(Ax))
+	Ax = ar.xorit(Ax, x)
 	Ax.reverse()
 	return Ax
 
 
-def sBox_gen():
+def sBox_gen(inv):
 	""" generate sBox of AES with subByte function trying all combinations
 
 	returns sBoxes
@@ -303,42 +331,57 @@ def sBox_gen():
 
 	s_box = []
 
-	for x in range(256):
+	for x in range(2):
 		x = int_to_bitarray(x)
 		x = ar.fill(x)
-		s_box.append(subBytes(x))
+		x = subBytes(x, inv)
+		s_box.append(x)
 
 	return s_box
 
 
-def shiftRows(a):
+def shiftRows(a, inv):
 	""" shift each row of a Nr times
 
 	return a shifted
 	"""
 
-	for x in range(len(a)):
-		a[x] = a[x][x:] + a[x][:x]
+	if not inv:
+
+		for x in range(0, len(a), 4):
+			a[x:x+4] = a[x:x+4][x//4:] + a[x:x+4][:x//4]
+
+	else:
+		for x in range(len(a), 0, -4):
+			a[x:x+4] = a[x:x+4][4-(x//4):] + a[x:x+4][:4-(x//4)]
 
 	return a
 
 
-def mixColumns(a):
+def mixColumns(a, inv):
 	""" linear function to diffus input
 
 	return a mixed
 	"""
 
-	B = [[0,0,0,0]]*4
+	B = [0]*16
 
-	for i in range(4):
+	if not inv:
 
-			B[i] = ar.reduct(ar.xorit(ar.xorit(ar.xorit(ar.mult(a[i], MC[0][0]), ar.mult(a[i + 4], MC[0][1])), ar.mult(a[i + 8], MC[0][2])), ar.mult(a[i + 12], MC[0][3])))
-			B[i + 4] = ar.reduct(ar.xorit(ar.xorit(ar.xorit(ar.mult(a[i], MC[1][0]), ar.mult(a[i + 4], MC[1][1])), ar.mult(a[i + 8], MC[1][2])), ar.mult(a[i + 12], MC[1][3])))
-			B[i + 8] = ar.reduct(ar.xorit(ar.xorit(ar.xorit(ar.mult(a[i], MC[2][0]), ar.mult(a[i + 4], MC[2][1])), ar.mult(a[i + 8], MC[2][2])), ar.mult(a[i + 12], MC[2][3])))
-			B[i + 12] = ar.reduct(ar.xorit(ar.xorit(ar.xorit(ar.mult(a[i], MC[3][0]), ar.mult(a[i + 4], MC[3][1])), ar.mult(a[i + 8], MC[3][2])), ar.mult(a[i + 12], MC[3][3])))
+		for i in range(4):
 
-	print(hex(int(concatenate_list_data(B),2)))
+				B[i] = ar.reduct(ar.xorit(ar.xorit(ar.xorit(ar.mult(a[i], MC[0][0]), ar.mult(a[i + 4], MC[0][1])), ar.mult(a[i + 8], MC[0][2])), ar.mult(a[i + 12], MC[0][3])))
+				B[i + 4] = ar.reduct(ar.xorit(ar.xorit(ar.xorit(ar.mult(a[i], MC[1][0]), ar.mult(a[i + 4], MC[1][1])), ar.mult(a[i + 8], MC[1][2])), ar.mult(a[i + 12], MC[1][3])))
+				B[i + 8] = ar.reduct(ar.xorit(ar.xorit(ar.xorit(ar.mult(a[i], MC[2][0]), ar.mult(a[i + 4], MC[2][1])), ar.mult(a[i + 8], MC[2][2])), ar.mult(a[i + 12], MC[2][3])))
+				B[i + 12] = ar.reduct(ar.xorit(ar.xorit(ar.xorit(ar.mult(a[i], MC[3][0]), ar.mult(a[i + 4], MC[3][1])), ar.mult(a[i + 8], MC[3][2])), ar.mult(a[i + 12], MC[3][3])))
+
+	else:
+		for i in range(4):
+
+				B[i] = ar.reduct(ar.xorit(ar.xorit(ar.xorit(ar.mult(a[i], MC_inv[0][0]), ar.mult(a[i + 4], MC_inv[0][1])), ar.mult(a[i + 8], MC_inv[0][2])), ar.mult(a[i + 12], MC_inv[0][3])))
+				B[i + 4] = ar.reduct(ar.xorit(ar.xorit(ar.xorit(ar.mult(a[i], MC_inv[1][0]), ar.mult(a[i + 4], MC_inv[1][1])), ar.mult(a[i + 8], MC_inv[1][2])), ar.mult(a[i + 12], MC_inv[1][3])))
+				B[i + 8] = ar.reduct(ar.xorit(ar.xorit(ar.xorit(ar.mult(a[i], MC_inv[2][0]), ar.mult(a[i + 4], MC_inv[2][1])), ar.mult(a[i + 8], MC_inv[2][2])), ar.mult(a[i + 12], MC_inv[2][3])))
+				B[i + 12] = ar.reduct(ar.xorit(ar.xorit(ar.xorit(ar.mult(a[i], MC_inv[3][0]), ar.mult(a[i + 4], MC_inv[3][1])), ar.mult(a[i + 8], MC_inv[3][2])), ar.mult(a[i + 12], MC_inv[3][3])))
 
 	return B
 
@@ -348,7 +391,6 @@ def KSC(B, i, s_box):
 
 	returns key rounded
 	"""
-
 	t = B
 	t = t[8:] + t[:8]
 
@@ -362,7 +404,9 @@ def KSC(B, i, s_box):
 
 	r = int_to_bitarray(2 ** (i - 1))
 
-	r = ar.xorit(t,ar.fill(r))
+	r = ar.reduct(r)
+
+	r = ar.xorit(t,r)	
 
 	return r + res
 
@@ -370,81 +414,173 @@ def KSC(B, i, s_box):
 def key_gen(s_box):
 	""" key generation 
 
-	returns expaned key
+	returns expanedd key
 	"""
 
+	EK = [K[0] + K[1] + K[2] + K[3], K[4] + K[5] + K[6] + K[7], K[8] + K[9] + K[10] + K[11], K[12] + K[13] + K[14] + K[15]]
+
+
+	t_p = EK[-1]
 	Nk = 4
-	EK = [K[0] + K[4] + K[8] + K[12], K[1] + K[5] + K[9] + K[13], K[2] + K[6] + K[10] + K[14], K[3] + K[7] + K[11] + K[15]]
-	t_pp = K[3] + K[7] + K[11] + K[15] 
 
 	for x in range(4, 44, 4):
-			t = KSC(t_pp, (x//Nk), s_box)
-			t = ar.xorit(t,EK[x-Nk])
+
+		t = KSC(t_p, (x//Nk), s_box)
+		t = ar.xorit(t, EK[x-Nk])
+		EK.append(t)
+		#print("k -->> " + str(hex(int(concatenate_list_data(t),2))[2:]) + ";")
+
+		for i in range(1,4):
+			t = ar.xorit(t, EK[x+i-Nk])
 			EK.append(t)
+			#print("k -->> " + str(hex(int(concatenate_list_data(t),2))[2:]) + ";")
+			t_p = t
+		#print("\n\n")
 
-			for i in range(3):
-				print("i -->> " + str(i))
-				print("x -->> " + str(x))
-				print("pos -->> " + str(x+i-Nk))
-				print("len EK -->> " + str(len(EK)) + "\n")
-				t = ar.xorit(t,EK[x+i-Nk])
-				EK.append(t)
-				t_pp = t
+	tmp = []
+	for x in range(0, 44, 4):
+		t = []
 
-	return EK
+		for i in range(4):
+			n = [EK[x + i][:8], EK[x + i][8:][:8], EK[x + i][16:][:8], EK[x + i][24:][:8]]
+			t.append(n)
+
+		tmp.append(t)
+
+
+	return tmp
 
 
 def addRoundKey(state, key):
 
 	for x in range(len(state)):
+		state[x] = ar.xorit(state[x], key[x%4][x//4])
+
+	return state
+
 		
-
-
 def AES(input, s_box):
 	""" AES function, AES implementation
 
-	returns AES output
+	returns AES ciphered output
 	"""
+
+	state = [	input[0], input[4], input[8], input[12],
+				input[1], input[5], input[9], input[13],
+				input[2], input[6], input[10], input[14],
+				input[3], input[7], input[11], input[15]]
 
 	ek = key_gen(s_box)
 
-	state = []
+	state = addRoundKey(state, ek[0])
 
-
-
-	#addRoundKey
-
-	for i in range(10):
-
+	for i in range(1,10):
 		s = []
 
-		for row in state:
-			s.append(subBytes(row))
+		for b in state:
+			s.append(subBytes(b, False))
 
-		s = shiftRows(s)
-		s = mixColumns(s)
+		s = shiftRows(s, False)
 
-		#addRoundKey
+		s = mixColumns(s, False)
 
-		state = s
+		state = addRoundKey(s, ek[i])
 
 	s = []
 
 	for row in state:
-		s.append(subBytes(row))
-	
-	s = shiftRows(s)
+		s.append(subBytes(row, False))
 
-	#addRoundKey
+	s = shiftRows(s, False)
 
-	return s
+	s = addRoundKey(s, ek[10])
+
+	state = [	s[0], s[4], s[8], s[12],
+				s[1], s[5], s[9], s[13],
+				s[2], s[6], s[10], s[14],
+				s[3], s[7], s[11], s[15]]
+
+	for b in state:
+		print("b -->> " + str(10) + " : " + str(hex(int(concatenate_list_data(b),2))[2:]) + ";")
+
+	print("\n\n")
+
+	return state
+
+
+def dAES(input, s_box):
+	""" AES function, AES implementation
+
+	returns AES clear output
+	"""
+
+	state = [	input[0], input[4], input[8], input[12],
+				input[1], input[5], input[9], input[13],
+				input[2], input[6], input[10], input[14],
+				input[3], input[7], input[11], input[15]]
+
+	ek = key_gen(s_box)
+
+	state = addRoundKey(state, ek[0])
+
+	for i in range(1,10):
+		s = []
+
+		state = shiftRows(state, True)
+
+		for b in state:
+			s.append(subBytes(b, True))
+
+		s = mixColumns(s, True)
+
+		state = addRoundKey(s, ek[i])
+
+	s = []
+
+	state = shiftRows(state, True)
+
+	for row in state:
+		s.append(subBytes(row, True))
+
+	s = addRoundKey(s, ek[10])
+
+	state = [	s[0], s[4], s[8], s[12],
+				s[1], s[5], s[9], s[13],
+				s[2], s[6], s[10], s[14],
+				s[3], s[7], s[11], s[15]]
+
+	for b in state:
+		print("b -->> " + str(10) + " : " + str(hex(int(concatenate_list_data(b),2))[2:]) + ";")
+
+	print("\n\n")
+
+	return state
+
 
 def main():
-	input = "10"*64 #128
+	input = [	ar.fill(int_to_bitarray(int("54",16))), ar.fill(int_to_bitarray(int("77",16))), ar.fill(int_to_bitarray(int("6F",16))), ar.fill(int_to_bitarray(int("20",16))),
+				ar.fill(int_to_bitarray(int("4F",16))), ar.fill(int_to_bitarray(int("6E",16))), ar.fill(int_to_bitarray(int("65",16))), ar.fill(int_to_bitarray(int("20",16))),
+				ar.fill(int_to_bitarray(int("4E",16))), ar.fill(int_to_bitarray(int("69",16))), ar.fill(int_to_bitarray(int("6E",16))), ar.fill(int_to_bitarray(int("65",16))),
+				ar.fill(int_to_bitarray(int("20",16))), ar.fill(int_to_bitarray(int("54",16))), ar.fill(int_to_bitarray(int("77",16))), ar.fill(int_to_bitarray(int("6F",16)))]
+	
+	"""s_box = sBox_gen(False)
 
-	s_box = sBox_gen()
+	ciphredText = AES(input, s_box)
 
-	AES(input, s_box)
+	print(ciphredText)"""
+
+	s_box = sBox_gen(True)
+
+	print(s_box)
+
+	for b in s_box:
+		print("sb -->> " + str(hex(int(concatenate_list_data(b),2))[2:]) + ";")
+
+	print("\n\n")
+
+	"""clearText = dAES(ciphredText, s_box)
+
+	print(ciphredText)"""
 
 
 if __name__ == "__main__":
